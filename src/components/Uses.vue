e86d18dba4c4ea93faa6c61a735a3752bfc9f9d2
unknown <yodsapatamon@gmail.com>
2025-09-09 18:33:51 +0700

    :)

--- src/components/Uses.vue
+++ src/components/Uses.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="mb-2 font-black text-2xl">uses/</div>
-  <img src="https://skillicons.dev/icons?i=apple,linux,git,vscode,github,discord,github" class="select-none mb-2" />
-  <img src="https://skillicons.dev/icons?i=c,python,go,js,typescript,react,vuejs" class="select-none" />
+  <img src="https://skillicons.dev/icons?i=apple,linux,git,vscode,discord,github" class="select-none mb-2" />
+  <img src="https://skillicons.dev/icons?i=c,python,go,js,typescript,vuejs" class="select-none" />
 </template>
